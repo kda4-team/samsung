3ce3b57696dfb47b23e34e60c0dc6efe01331e73
jyleo2k2 <jyleo2k2@gmail.com>
2026-07-06 11:21:49 +0900

    main 파일 만듦

--- __main__.py
+++ __main__.py
@@ -1,2 +1,6 @@
-from samsung import *
+from . import *
 
+print(f'add(10+5):{add(10,5)}')
+print(f'minus(10-5):{minus(10,5)}')
+print(f'multiplication(10*5):{multiplication(10,5)}')
+print(f'division(10/5):{division(10,5)}')
